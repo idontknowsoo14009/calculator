b81e61c2f7e2931e53e07b711512f8f8a17ce001
idontknowsoo14009 <146782772+idontknowsoo14009@users.noreply.github.com>
2024-06-19 14:03:22 -0700

    Update button.js
     I gave some comments if u don't understand the code
    yes I googled it but I understand what this code is doing

--- button.js
+++ button.js
@@ -1,58 +1,60 @@
-const display = document.getElementById('display'); 
-let firstValue = null; 
+const display = document.getElementById('display'); // just so you know these comments are made by me and yes i know i googled it 
+let firstValue = null; // but I know what the code does,  this code creates variables the first one looks for an input element in the html file
 let operator = null; 
 let waitingforSecondoperator = false; 
 
 function updateDisplay(value) {
-    display.value = value;
+    display.value = value; // this checks for the input value and will also update the text in the input textbox
   }
   
   function handleNumber(number) {
     if (waitingForSecondValue) {
-      updateDisplay(number);
-      waitingForSecondValue = false;
+      updateDisplay(number); // this function needs to be applied to the button onclick
+      waitingForSecondValue = false; // and in the () you have to put a number in the html file
     } else {
-      const currentValue = display.value;
-      updateDisplay(currentValue === '0' ? number : currentValue + number);
-    }
+      const currentValue = display.value; // this gets the current value in the input html element
+      updateDisplay(currentValue === '0' ? number : currentValue + number); 
+    } // current value=== 0 checks if the current value is the same as zero 
+      // this is also a if else statement and if currentvalue===0 is true then the conditon after the first colon is evualeted
+      
   }
   
-  function handleOperator(op) {
-    const currentValue = parseFloat(display.value);
+  function handleOperator(op) { // handle operator needs to be applied to button onclick and button needs to be an operator
+    const currentValue = parseFloat(display.value); // and in the () u have to put an operator but with an string like ''
     if (operator === null) {
       firstValue = currentValue;
     } else if (waitingForSecondValue) {
       performCalculation();
     }
-    operator = op;
-    waitingForSecondValue = true;
+    operator = op; // op stands for operator i think
+    waitingForSecondValue = true; // and this changes the variable's value to true(boolean)
   }
   
-  function performCalculation() {
+  function performCalculation() { // this function needs to be applied to the enter button onclick in html file
     const secondValue = parseFloat(display.value);
-    let result;
+    let result; // and this does operations, if it was  + then it will do first vl=alue + secondvalue
     switch (operator) {
       case '+':
-        result = firstValue + secondValue;
+        result = firstValue + secondValue; // this does the operation if + was pressed
         break;
       case '-':
-        result = firstValue - secondValue;
+        result = firstValue - secondValue; // if the user did subtraction then it would do this
         break;
       case '*':
-        result = firstValue * secondValue;
+        result = firstValue * secondValue; // it will multiply the first and second value if the user did multipaction
         break;
       case '/':
-        result = firstValue / secondValue;
+        result = firstValue / secondValue; // it will divide the first and second value if the user did divison
         break;
       default:
-        return;
+        return; 
     }
-    operator = null;
+    operator = null; // i dont have to explain this
     firstValue = result;
     updateDisplay(result);
   }
   
-  function handleClear() {
+  function handleClear() { // this needs to be applied to clear button onclick in html
     updateDisplay('0');
     firstValue = null;
     operator = null;
